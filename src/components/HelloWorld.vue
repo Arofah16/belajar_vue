1e9f9b980d03aecfe80c1a6dd0ac80130b149544
arofah16 <arofahr06@gmail.com>
2020-12-15 13:31:35 +0700

    belajar 01

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- <h1>{{mesage}}</h1> -->
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >
+    </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+// var app = new Vue({
+//   el:"#app",
+//   data:{
+//     message:"hallo semangat arofah"
+//   }
+// })
+</script>
+
