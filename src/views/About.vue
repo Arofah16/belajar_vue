de2599d8a085daa5cc187f704fb0fe27b37f45e1
arofah16 <arofahr06@gmail.com>
2020-12-16 17:56:17 +0700

    push

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{data}}</h1>
+    <h3> membuat direktif v-html: <span v-html="rawHtml"></span></h3>
+    <button v-bind:id="dynamicId">v-bind</button>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        dynamicId:true,
+        data: "Semangat Arofah"
+      }
+    }
+  }
+</script>
