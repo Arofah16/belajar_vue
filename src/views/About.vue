00165c33f8e90d86b6f7457c5839fde1c13c7b50
arofah16 <arofahr06@gmail.com>
2020-12-30 17:35:48 +0700

    v-if

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,23 +12,38 @@
     <!-- <ul>
       <li v-for="(k,index) of lessons" >{{index+1}}-{{k}}</li>
     </ul> -->
-    <h3>mencari bilangan genap dengan menggunakan v-for(latihan v-for) {{numbers}}</h3>
+    <!-- <h3>mencari bilangan genap dengan menggunakan v-for(latihan v-for) {{numbers}}</h3>
     <ul>
       <li v-for="num in cariBilanganGenap(numbers)" :key="num">{{num}}</li>
-    </ul>
+    </ul> -->
+    <h2>membuat daftar pelajaran dengan merender menggunakan v-if {{lessons.length}}</h2>
+    <template  v-if="lessons.length > 1">
+      <ul>
+        <li v-for="(lesson,index) of lessons" :key="lesson">{{index+1}}--{{lesson}}</li>
+      </ul>
+    </template>
+    <!-- <li v-else> data pelajaran tidak ada</li> -->
+ <ul>
+   <li v-for="value in hari" :key="value">{{value}}</li>
+ </ul>
   </div>
 </template>
 <script>
   export default {
-    data(){
-      return{
-        lessons:['filsafat','psikologi','sosiologi'],
-        numbers:[1,2,3,4,5,6,7],
+    data() {
+      return {
+        lessons: ['filsafat', 'psikologi', 'sosiologi'],
+        numbers: [1, 2, 3, 4, 5, 6, 7],
+        hari:{
+          satu:'monday',
+          dua:'tuesday'
+        }
+        
       }
     },
-    methods:{
-      cariBilanganGenap: function(data){
-        return data.filter(angka =>{
+    methods: {
+      cariBilanganGenap: function (data) {
+        return data.filter(angka => {
           return angka % 2 == 0
         })
       }
